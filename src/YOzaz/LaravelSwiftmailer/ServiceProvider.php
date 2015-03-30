306d8a33ed1fb6e5ee6e68ca34740db0c0ee4dc1
Marijus Plančiūnas <marijus@sheologydigital.com>
2015-03-30 16:35:34 +0100

    Initial commit

---
+++ src/YOzaz/LaravelSwiftmailer/ServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace YOzaz\LaravelSwiftmailer;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('yozaz/laravel-swiftmailer');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('mailer');
+	}
+
+}
